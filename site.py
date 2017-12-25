7948f6b3bc49b499f24108092355cde39415eb19
Ron Shemesh <ron.shemesh98@gmail.com>
2017-12-26 01:22:45 +0200

    little change

--- site.py
+++ site.py
@@ -129,10 +129,8 @@ def get_similar_to_site_and_file(wanted, algo, f):
     global num_results
     text = ""
     if algo not in words_counters_dict.keys():
-        print('regular sort')
         inds, sims = top_similar(wanted, vectors_dict[algo], results_to_show=num_results)
     else:
-        print('smart sort')
         inds, sims = top_similar_smart(wanted, vectors_dict[algo], words_counters_dict[algo],
                                        results_to_show=num_results)
     for i in range(len(inds)):
