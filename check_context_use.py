f60148d915068e33568601ecb73954ebcbdb18ff
Ron Shemesh <ron.shemesh98@gmail.com>
2017-12-28 22:40:02 +0200

    play with context

--- check_context_use.py
+++ check_context_use.py
@@ -18,13 +18,14 @@ def get_context_vec(path, context_words, words_list):
 
 
 if __name__ == '__main__':
-    with open(join("result", Path.path_w2v_nn_pos_200.value, "words_list.txt"), 'r') as f:
+    path = Path.path_w2v_neg_20.value
+    with open(join("result", path, "words_list.txt"), 'r') as f:
         cur_words = f.readlines()
     words = [word[:-1] for word in cur_words]
-    output_vecs = np.load(join("result", Path.path_w2v_nn_pos_200.value, "words_vectors.npy"))
-    context = get_context_vec(join('result', Path.path_w2v_nn_pos_200.value), ["חולים", "כנסת", "עקרת", "ספר~תיכון"],
+    output_vecs = np.load(join("result", path, "words_vectors.npy"))
+    context = get_context_vec(join('result', path), ["חולים", "כנסת", "עקרת", "ספר~תיכון"],
                               words)
-    context_vectors = np.load(join(join('result', Path.path_w2v_nn_pos_200.value), "context_vectors.npy"))
+    context_vectors = np.load(join('result', path, "context_vectors.npy"))
 
     print get_similar_console(context, words,
                               # context_vectors)
