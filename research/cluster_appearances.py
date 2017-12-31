2e1cd0c6edb88f20973749332751d5d4b0bbb719
Ron Shemesh <ron.shemesh98@gmail.com>
2018-01-01 01:37:44 +0200

    try to find bug

--- research/cluster_appearances.py
+++ research/cluster_appearances.py
@@ -43,6 +43,8 @@ def evaluate(kmeans):
     test_set = load_test_set()
     predicted_labels = kmeans.predict(test_set['x'])
     true_labels = test_set['y']
+    print len(true_labels)
+    print len(test_set['x'])
     if accuracy_score(true_labels, predicted_labels) < 0.5:
         predicted_labels = [1 - l for l in predicted_labels]
     print sum(predicted_labels)
