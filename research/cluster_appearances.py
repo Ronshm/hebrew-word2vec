77032c3961af7439e66d662485f50c39fd2f9380
Ron Shemesh <ron.shemesh98@gmail.com>
2018-01-05 16:16:32 +0200

    run tests

--- research/cluster_appearances.py
+++ research/cluster_appearances.py
@@ -6,23 +6,25 @@ from local_utils import *
 
 
 def part_one(wanted_word):
-    if exists(join('data', 'windows-vecs.npy')):
-        appearances_map = read_appearances_map_from_file(join('data', 'appearance_map.txt'))
-        windows_vecs = np.load(join('data', 'windows-vecs.npy'))
+    new_word_data_path = join('data', wanted_word)
+    if exists(join(new_word_data_path, 'windows-vecs.npy')):
+        appearances_map = read_appearances_map_from_file(join(new_word_data_path, 'appearance_map.txt'))
+        windows_vecs = np.load(join(new_word_data_path, 'windows-vecs.npy'))
         return appearances_map, windows_vecs
     appearances_map = get_appearances_map(wanted_word)
-    write_appearances_map_to_file(appearances_map, join('data', 'appearance_map.txt'))
+    write_appearances_map_to_file(appearances_map, join(new_word_data_path, 'appearance_map.txt'))
     d = get_w2v_dict('data')
     windows_vecs = create_windows_vecs(appearances_map, d, wanted_word)
-    np.save(join('data', 'windows-vecs.npy'), windows_vecs)
+    np.save(join(new_word_data_path, 'windows-vecs.npy'), windows_vecs)
     return appearances_map, windows_vecs
 
 
-def part_two(appearance_map, windows_vecs):
+def part_two(wanted_word, appearance_map, windows_vecs):
+    new_word_data_path = join('data', wanted_word)
     kmeans = KMeans(n_clusters=2, random_state=0).fit(windows_vecs)
     labels = kmeans.labels_
-    f1 = open(join('data', 'cluster1_sentences.txt'), 'w')
-    f2 = open(join('data', 'cluster2_sentences.txt'), 'w')
+    f1 = open(join(new_word_data_path, 'cluster1_sentences.txt'), 'w')
+    f2 = open(join(new_word_data_path, 'cluster2_sentences.txt'), 'w')
     cluster1_windows = []
     cluster2_windows = []
     for i, win in enumerate(appearance_map):
@@ -52,11 +54,18 @@ def evaluate(kmeans):
     print "precision class 2:", precision_score(true_labels, predicted_labels, pos_label=1)
 
 
-def main():
-    appearances_map, windows_vecs = part_one('שמים')
-    kmeans = part_two(appearances_map, windows_vecs)
+def main(word):
+    appearances_map, windows_vecs = part_one(word)
+    kmeans = part_two(word, appearances_map, windows_vecs)
     # evaluate(kmeans)
 
 
 if __name__ == '__main__':
-    main()
+    main("בצל")
+    main("שמים")
+    main("שלווה")
+    main("שלב")
+    main("שרק")
+    main("שטף")
+    main("בטח")
+    main("בנים")
